f112d6ffb1da550585fbeca1034335201e62c13a
Ashraf <ahmedashraaf09@gmail.com>
2023-10-23 01:50:47 +0300

    Refactoring EditJob

--- Object Repository/EditJob/Page_Job title new test after edit - NAILS _02945b/h1_Job title new test after edit.rs
+++ Object Repository/EditJob/Page_Job title new test after edit - NAILS _02945b/h1_Job title new test after edit.rs
@@ -6,12 +6,12 @@
    <elementGuidId>b8c1ae5c-2e3f-43c5-8974-d4b02f4fa1fd</elementGuidId>
    <selectorCollection>
       <entry>
-         <key>XPATH</key>
-         <value>(.//*[normalize-space(text()) and normalize-space(.)='Job title new test after edit'])[1]/following::h1[1]</value>
-      </entry>
-      <entry>
          <key>CSS</key>
          <value>h1</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>(//td /div /a)[1]</value>
       </entry>
    </selectorCollection>
    <selectorMethod>XPATH</selectorMethod>
@@ -22,7 +22,7 @@
       <name>tag</name>
       <type>Main</type>
       <value>h1</value>
-      <webElementGuid>9cb99586-6fa2-4e1b-9bc8-4bab8b72956e</webElementGuid>
+      <webElementGuid>4274945d-726a-4ec2-b3cf-f5e3cffda353</webElementGuid>
    </webElementProperties>
    <webElementProperties>
       <isSelected>true</isSelected>
@@ -30,7 +30,7 @@
       <name>text</name>
       <type>Main</type>
       <value>Job title new test after edit</value>
-      <webElementGuid>8d31d726-aaa0-46a2-b536-467942d9f10a</webElementGuid>
+      <webElementGuid>ffb8413b-6770-41a0-ae87-09b326c3565e</webElementGuid>
    </webElementProperties>
    <webElementProperties>
       <isSelected>false</isSelected>
@@ -38,7 +38,7 @@
       <name>xpath</name>
       <type>Main</type>
       <value>/html[1]/body[@class=&quot;inner--page viewjob-page&quot;]/section[@class=&quot;view-job-content&quot;]/div[@class=&quot;page-wrapper&quot;]/div[@class=&quot;view-job-content-inner&quot;]/div[@class=&quot;job-detail-head&quot;]/div[@class=&quot;job-details-head-inner&quot;]/div[@class=&quot;job-title display--flex flex--wrap&quot;]/h1[1]</value>
-      <webElementGuid>387cbf27-b9df-489b-9bad-142870924018</webElementGuid>
+      <webElementGuid>8bbfc792-64a2-41ec-aee9-ff5dcc6d14dc</webElementGuid>
    </webElementProperties>
    <webElementXpaths>
       <isSelected>true</isSelected>
@@ -46,7 +46,7 @@
       <name>xpath:neighbor</name>
       <type>Main</type>
       <value>(.//*[normalize-space(text()) and normalize-space(.)='Job title new test after edit'])[1]/following::h1[1]</value>
-      <webElementGuid>117c382d-2a4a-4aa0-b85e-0f8627236711</webElementGuid>
+      <webElementGuid>1e57d3b8-9981-4c84-9bff-780c4ba7eb46</webElementGuid>
    </webElementXpaths>
    <webElementXpaths>
       <isSelected>false</isSelected>
@@ -54,7 +54,7 @@
       <name>xpath:neighbor</name>
       <type>Main</type>
       <value>(.//*[normalize-space(text()) and normalize-space(.)='Airline'])[1]/following::h1[1]</value>
-      <webElementGuid>ab6afd5e-5d6a-4942-b16c-1fa54a135f5a</webElementGuid>
+      <webElementGuid>ecb81329-8f9a-40b2-94aa-4a4aa2715e6f</webElementGuid>
    </webElementXpaths>
    <webElementXpaths>
       <isSelected>false</isSelected>
@@ -62,7 +62,7 @@
       <name>xpath:neighbor</name>
       <type>Main</type>
       <value>(.//*[normalize-space(text()) and normalize-space(.)='City &amp; Country'])[1]/preceding::h1[1]</value>
-      <webElementGuid>837b6458-56c7-49b7-8d51-19780730af53</webElementGuid>
+      <webElementGuid>66530df9-c4f7-4f6f-ba27-c13ee8f8a204</webElementGuid>
    </webElementXpaths>
    <webElementXpaths>
       <isSelected>false</isSelected>
@@ -70,7 +70,7 @@
       <name>xpath:neighbor</name>
       <type>Main</type>
       <value>(.//*[normalize-space(text()) and normalize-space(.)='Temporary'])[1]/preceding::h1[1]</value>
-      <webElementGuid>68037729-750b-4117-973b-08a15991520f</webElementGuid>
+      <webElementGuid>099eedc9-067a-4aae-ab1e-28024fb42449</webElementGuid>
    </webElementXpaths>
    <webElementXpaths>
       <isSelected>false</isSelected>
@@ -78,7 +78,7 @@
       <name>xpath:position</name>
       <type>Main</type>
       <value>//h1</value>
-      <webElementGuid>a93b43e5-aa85-4cfb-ae8f-685402e60b20</webElementGuid>
+      <webElementGuid>0347a8fe-fd34-4e82-9608-a23ca20f953d</webElementGuid>
    </webElementXpaths>
    <webElementXpaths>
       <isSelected>false</isSelected>
@@ -86,6 +86,6 @@
       <name>xpath:customAttributes</name>
       <type>Main</type>
       <value>//h1[(text() = 'Job title new test after edit' or . = 'Job title new test after edit')]</value>
-      <webElementGuid>c2e2aa7e-f283-4d12-96cb-30e9ee78ca9d</webElementGuid>
+      <webElementGuid>0284ac22-791e-4ced-a059-09b90f8f8cb5</webElementGuid>
    </webElementXpaths>
 </WebElementEntity>
