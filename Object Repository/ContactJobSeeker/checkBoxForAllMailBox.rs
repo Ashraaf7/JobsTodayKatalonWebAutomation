9e8391dc2c0c596da3b5165429ea7aae465cab33
Ashraf <ahmedashraaf09@gmail.com>
2023-10-28 00:25:38 +0200

    Delete a duplicated file

---
+++ Object Repository/ContactJobSeeker/checkBoxForAllMailBox.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>checkBoxForAllMailBox</name>
+   <tag></tag>
+   <elementGuidId>504739c8-1dd7-4b81-9f73-c948d9fd3734</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@class='all-select-box'] /input</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
---
+++ Object Repository/ContactJobSeeker/deleteAllMailBoxes.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>deleteAllMailBoxes</name>
+   <tag></tag>
+   <elementGuidId>90f14f03-1f0b-4151-b0ef-b232c910084f</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//a[@id=&quot;mail-delete-selected&quot;]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
