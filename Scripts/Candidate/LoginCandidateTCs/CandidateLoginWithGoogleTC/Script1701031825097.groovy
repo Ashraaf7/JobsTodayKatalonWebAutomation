49fc16ea8a87a7cc2bfb8cd18438c004e147b4d9
Ashraf <ahmedashraaf09@gmail.com>
2023-11-27 17:55:19 +0200

    updating gitignore file again

---
+++ Scripts/Candidate/LoginCandidateTCs/CandidateLoginWithGoogleTC/Script1701031825097.groovy
@@ -0,0 +1,59 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+import org.openqa.selenium.interactions.Actions as Actions
+import com.kms.katalon.core.webui.driver.DriverFactory as DriverFactory
+import com.kms.katalon.core.webui.driver.DriverFactory
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.WebDriver
+import org.openqa.selenium.chrome.ChromeDriver
+import org.openqa.selenium.chrome.ChromeOptions
+import org.openqa.selenium.edge.EdgeDriver
+import org.openqa.selenium.edge.EdgeOptions
+
+// Copy the path to chromedriver.exe
+String pathToChromeDriver = "C:\\Users\\elost\\.katalon\\packages\\Katalon_Studio_Windows_64-8.6.5\\configuration\\resources\\drivers\\edgechromium_win64\\msedgedriver.exe"
+System.setProperty("webdriver.edge.driver", pathToChromeDriver)
+
+// It is important that this chromeProfilePath ends with User Data and not with the profile folder (Profile 2)
+String chromeProfilePath = "C:\\Users\\elost\\AppData\\Local\\Microsoft\\Edge\\User Data";
+EdgeOptions edge = new EdgeOptions();
+edge.setCapability("user-data-dir", chromeProfilePath)
+// Here you specify the actual profile folder (Profile 2)
+edge.setCapability("profile-directory","Default");
+
+WebDriver driver = new EdgeDriver(edge);
+driver.get("https://gmail.com/");
+DriverFactory.changeWebDriver(driver)
+
+// Here you can continue your test case
+/*
+
+WebUI.navigateToUrl(GlobalVariable.Production)
+
+
+WebUI.maximizeWindow()
+
+if (WebUI.verifyElementVisible(findTestObject('Object Repository/regCandElements/Page_Jobs Search, Search for a Job - Jobsto_ce5d69/button_Accept All Cookies'), FailureHandling.OPTIONAL))
+	WebUI.click(findTestObject('Object Repository/regCandElements/Page_Jobs Search, Search for a Job - Jobsto_ce5d69/button_Accept All Cookies'))
+
+WebUI.click(findTestObject('Object Repository/loginElements/Page_Jobs Search, Search for a Job - Jobsto_ce5d69/a_Login'))
+*/
