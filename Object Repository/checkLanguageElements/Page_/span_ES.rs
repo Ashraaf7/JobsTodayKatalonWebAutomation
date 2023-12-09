1978acc03e39e376e0824069a383a79e523ba9d6
Ashraf <ahmedashraaf09@gmail.com>
2023-12-09 22:09:44 +0200

    General/checkLanguageTC [DONE]

--- Object Repository/checkLanguageElements/Page_/span_ES.rs
+++ Object Repository/checkLanguageElements/Page_/span_ES.rs
@@ -6,15 +6,15 @@
    <elementGuidId>03e1c1b0-317e-40e8-8f80-accd684d0e2b</elementGuidId>
    <selectorCollection>
       <entry>
-         <key>XPATH</key>
-         <value>//*[@id=&quot;header&quot;]/div[3]/div[2]/ul/li[3]/div/div/div/div/span[3]</value>
+         <key>CSS</key>
+         <value>#languageMenu > ul > li:nth-child(3)</value>
       </entry>
       <entry>
-         <key>CSS</key>
-         <value></value>
+         <key>XPATH</key>
+         <value>#languageMenu > ul > li:nth-child(3)</value>
       </entry>
    </selectorCollection>
-   <selectorMethod>XPATH</selectorMethod>
+   <selectorMethod>CSS</selectorMethod>
    <useRalativeImagePath>true</useRalativeImagePath>
    <webElementProperties>
       <isSelected>false</isSelected>
@@ -22,7 +22,7 @@
       <name>tag</name>
       <type>Main</type>
       <value>span</value>
-      <webElementGuid>395cc0e0-5531-45fc-99cf-96648289aef9</webElementGuid>
+      <webElementGuid>5ee41159-0747-48a0-8630-8d39b188eecb</webElementGuid>
    </webElementProperties>
    <webElementProperties>
       <isSelected>false</isSelected>
@@ -30,7 +30,7 @@
       <name>class</name>
       <type>Main</type>
       <value>custom-option undefined</value>
-      <webElementGuid>536d5e00-6fcc-4dbf-a745-15f57e0f7c1e</webElementGuid>
+      <webElementGuid>9e77fa66-5c85-455e-bb11-23b0f523703e</webElementGuid>
    </webElementProperties>
    <webElementProperties>
       <isSelected>false</isSelected>
@@ -38,7 +38,7 @@
       <name>data-value</name>
       <type>Main</type>
       <value>/es/ofertas-de-empleo//?location=Cairo%2C+Egypt&amp;posted_date=all_jobs&amp;customsize=16</value>
-      <webElementGuid>7cb9dc33-530b-4a55-83fd-9e077761c853</webElementGuid>
+      <webElementGuid>e8ac2579-a201-4b9f-a582-12a71330d445</webElementGuid>
    </webElementProperties>
    <webElementProperties>
       <isSelected>true</isSelected>
@@ -47,7 +47,7 @@
       <type>Main</type>
       <value>
         ES    </value>
-      <webElementGuid>f7554567-83fa-4b09-88ad-3b82acc5032d</webElementGuid>
+      <webElementGuid>ba9811c5-8bae-46ff-98a9-43def5467709</webElementGuid>
    </webElementProperties>
    <webElementProperties>
       <isSelected>false</isSelected>
@@ -55,7 +55,7 @@
       <name>xpath</name>
       <type>Main</type>
       <value>id(&quot;header&quot;)/div[@class=&quot;header-menu&quot;]/div[@class=&quot;vertical-menu right--menu mobile-hidden&quot;]/ul[@class=&quot;display--flex align-item--center&quot;]/li[@class=&quot;language-dropdown&quot;]/div[@class=&quot;select-box select-language&quot;]/div[@class=&quot;custom-select-wrapper&quot;]/div[@class=&quot;custom-select-wrapper&quot;]/div[@class=&quot;custom-select opened&quot;]/div[@class=&quot;custom-options&quot;]/span[@class=&quot;custom-option undefined&quot;]</value>
-      <webElementGuid>1dc62dd6-5a7c-4bdc-aac5-8ecc41219961</webElementGuid>
+      <webElementGuid>0a987f59-7134-444a-9d77-91255c494e3f</webElementGuid>
    </webElementProperties>
    <webElementXpaths>
       <isSelected>true</isSelected>
@@ -63,7 +63,7 @@
       <name>xpath:idRelative</name>
       <type>Main</type>
       <value>//header[@id='header']/div[3]/div[2]/ul/li[3]/div/div/div/div/div/span[3]</value>
-      <webElementGuid>993ccb9e-9b3e-4a70-afb1-d4911677ebd7</webElementGuid>
+      <webElementGuid>0bbc72c7-6e9f-4e8f-99ce-4e5476b6b45e</webElementGuid>
    </webElementXpaths>
    <webElementXpaths>
       <isSelected>false</isSelected>
@@ -71,7 +71,7 @@
       <name>xpath:neighbor</name>
       <type>Main</type>
       <value>(.//*[normalize-space(text()) and normalize-space(.)='DE'])[4]/following::span[1]</value>
-      <webElementGuid>ab558e45-fe60-406d-8070-78f5729019c3</webElementGuid>
+      <webElementGuid>b4236b2d-37d3-40d4-ae6e-86bd8af22ec8</webElementGuid>
    </webElementXpaths>
    <webElementXpaths>
       <isSelected>false</isSelected>
@@ -79,7 +79,7 @@
       <name>xpath:neighbor</name>
       <type>Main</type>
       <value>(.//*[normalize-space(text()) and normalize-space(.)='EN'])[3]/following::span[2]</value>
-      <webElementGuid>01034995-2425-4b09-a510-ee59c673ed52</webElementGuid>
+      <webElementGuid>ecfb3632-99e2-4864-875b-c5721a73054e</webElementGuid>
    </webElementXpaths>
    <webElementXpaths>
       <isSelected>false</isSelected>
@@ -87,7 +87,7 @@
       <name>xpath:neighbor</name>
       <type>Main</type>
       <value>(.//*[normalize-space(text()) and normalize-space(.)='PL'])[3]/preceding::span[1]</value>
-      <webElementGuid>d141ac90-40d1-4aa6-8994-eabc489cf2e5</webElementGuid>
+      <webElementGuid>4d423aeb-527d-4d29-b704-61d8dd692c1d</webElementGuid>
    </webElementXpaths>
    <webElementXpaths>
       <isSelected>false</isSelected>
@@ -95,7 +95,7 @@
       <name>xpath:neighbor</name>
       <type>Main</type>
       <value>(.//*[normalize-space(text()) and normalize-space(.)='Suche'])[1]/preceding::span[2]</value>
-      <webElementGuid>fc486d32-c338-4a10-bbe1-16fcc31caf10</webElementGuid>
+      <webElementGuid>a8c65bd9-6fec-4b88-a5ff-cd49c2239c5a</webElementGuid>
    </webElementXpaths>
    <webElementXpaths>
       <isSelected>false</isSelected>
@@ -103,7 +103,7 @@
       <name>xpath:position</name>
       <type>Main</type>
       <value>//span[3]</value>
-      <webElementGuid>391ee000-bb86-49b5-9b06-d706f65f5e99</webElementGuid>
+      <webElementGuid>e4609335-f573-4199-a43a-e406c5d4d706</webElementGuid>
    </webElementXpaths>
    <webElementXpaths>
       <isSelected>false</isSelected>
@@ -113,6 +113,6 @@
       <value>//span[(text() = '
         ES    ' or . = '
         ES    ')]</value>
-      <webElementGuid>1c96f1fd-183d-4e74-9577-91e4fafea0b5</webElementGuid>
+      <webElementGuid>aca01f01-469a-49e2-b1a7-092866ccdcf3</webElementGuid>
    </webElementXpaths>
 </WebElementEntity>
